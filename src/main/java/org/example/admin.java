d07f3238704c43b3e2c05e6cfa61295364ac167b
deshithakavindu <deshithakavindu2729@gmail.com>
2025-09-30 23:53:20 +0530

    admin updates

---
+++ src/main/java/org/example/admin.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class admin {
+    String username ;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+    public void setStation(String station) {
+        this.station = station;
+    }
+
+    String station;
+}
