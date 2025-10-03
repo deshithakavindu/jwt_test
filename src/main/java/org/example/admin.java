965cc986cb50f7d07dfe6dd0d86b3d3ae9d7468d
deshithakavindu <deshithakavindu2729@gmail.com>
2025-10-03 16:30:50 +0530

    update admin.java

--- src/main/java/org/example/admin.java
+++ src/main/java/org/example/admin.java
@@ -1,23 +1,6 @@
 package org.example;
 
-public class admin {
-    String username ;
+public class Admin {
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getStation() {
-        return station;
-    }
-
-    public void setStation(String station) {
-        this.station = station;
-    }
-
-    String station;
+    String name ;
 }
